e486acdb109f5e2f9743e0f78e2d13206e64f814
sjack5354 <1462813013@qq.com>
2022-02-26 14:59:54 +0800

    sleep commit sleep2 method

--- src/person/Sleep.java
+++ src/person/Sleep.java
@@ -4,4 +4,7 @@ public class Sleep {
     public void sleep(){
         System.out.println("this is sleep method");
     }
+    public void sleep2(){
+        System.out.println("this is sleep2 method");
+    }
 }
